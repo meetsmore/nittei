5b9ea77ce481a7e265684a23c2c8e7fc9e9ea1d9
Guillaume Deconinck <deconinck.guillaume@meetsmore.com>
2024-09-30 18:46:06 +0900

    adapt jemalloc usage

--- bins/nittei/src/main.rs
+++ bins/nittei/src/main.rs
@@ -3,11 +3,14 @@ mod telemetry;
 use nittei_api::Application;
 use nittei_infra::setup_context;
 use telemetry::init_subscriber;
-
 // Use Jemalloc only for musl-64 bits platforms
 #[cfg(all(target_env = "musl", target_pointer_width = "64"))]
 #[global_allocator]
-static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
+use tikv_jemallocator::Jemalloc;
+
+#[cfg(all(target_env = "musl", target_pointer_width = "64"))]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
