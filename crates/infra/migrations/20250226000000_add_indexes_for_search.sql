b977f2aa7c23a49d54e19378ab2cee775b3742ee
Guillaume Deconinck <deconinck.guillaume@meetsmore.com>
2025-02-26 13:56:04 +0900

    chore: improve comments

--- crates/infra/migrations/20250226000000_add_indexes_for_search.sql
+++ crates/infra/migrations/20250226000000_add_indexes_for_search.sql
@@ -7,15 +7,15 @@ CREATE INDEX IF NOT EXISTS calendar_events__calendar_uid_event_type_status_start
 CREATE INDEX IF NOT EXISTS calendar_events__event_type_status_start_time ON calendar_events (event_type, status, start_time);
 
 -- Create an index on `calendar_uid` + `original_start_time` in the `calendar_events` table.
--- Useful for querying on specific users
+-- Useful for getting the recurring events' exceptions for specific users
 CREATE INDEX IF NOT EXISTS calendar_events__calendar_uid_original_start_time ON calendar_events (calendar_uid, original_start_time);
 
 -- Create an index on `recurring_event_uid` in the `calendar_events` table.
--- Useful for searching the exceptions of a recurring event
+-- Useful for directly searching the exceptions of a recurring event, without worrying about the `original_start_time` or user
 CREATE INDEX IF NOT EXISTS calendar_events__recurring_event_uid ON calendar_events (recurring_event_uid);
 
 -- Create an index on `calendar_uid` + `created` in the `calendar_events` table.
--- Useful for querying on specific users
+-- Useful for querying the most recent events for specific users
 CREATE INDEX IF NOT EXISTS calendar_events__calendar_uid_created ON calendar_events (calendar_uid, created);
 
 -- Create an index on `updated` in the `calendar_events` table.
