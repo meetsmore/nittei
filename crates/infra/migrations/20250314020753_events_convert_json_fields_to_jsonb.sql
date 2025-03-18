ac807c5d40334db4fe4d6068bed73127bb7c7b8f
Guillaume Deconinck <deconinck.guillaume@meetsmore.com>
2025-03-18 09:33:21 +0900

    fix: do step by step

--- crates/infra/migrations/20250314020753_events_convert_json_fields_to_jsonb.sql
+++ crates/infra/migrations/20250314020753_events_convert_json_fields_to_jsonb.sql
@@ -1,48 +1,9 @@
--- Add migration script here
 -- RECURRENCE
--- Convert recurrence field to JSONB
-ALTER TABLE
-  calendar_events
-ADD
-  COLUMN temp_recurrence_jsonb JSONB;
-
--- Update in batches to reduce locking time
-UPDATE
-  calendar_events
-SET
-  temp_recurrence_jsonb = CASE
-    -- Convert 'null' string to real NULL, as it's better for performance
-    WHEN recurrence::TEXT = 'null' THEN NULL
-    ELSE recurrence::JSONB
-  END
-WHERE
-  temp_recurrence_jsonb IS NULL;
-
--- Ensure no new writes happen before swapping columns
-ALTER TABLE
-  calendar_events DROP COLUMN recurrence;
-
-ALTER TABLE
-  calendar_events RENAME COLUMN temp_recurrence_jsonb TO recurrence;
+-- Add recurrence field with JSONB
+ALTER TABLE calendar_events
+ADD COLUMN temp_recurrence_jsonb JSONB;
 
 -- REMINDERS
--- Convert reminders field to JSONB
-ALTER TABLE
-  calendar_events
-ADD
-  COLUMN temp_reminders_jsonb JSONB;
-
--- Update in batches to reduce locking time
-UPDATE
-  calendar_events
-SET
-  temp_reminders_jsonb = reminders::JSONB
-WHERE
-  temp_reminders_jsonb IS NULL;
-
--- Ensure no new writes happen before swapping columns
-ALTER TABLE
-  calendar_events DROP COLUMN reminders;
-
-ALTER TABLE
-  calendar_events RENAME COLUMN temp_reminders_jsonb TO reminders;
+-- Add reminders field with JSONB
+ALTER TABLE calendar_events
+ADD COLUMN temp_reminders_jsonb JSONB;
