9347fce2ed51c80213b8b3ae9eb90b789e6d95f2
Guillaume Deconinck <deconinck.guillaume@meetsmore.com>
2025-03-21 18:39:27 +0900

    fix: increase limits

--- crates/api/src/account/account_search_events.rs
+++ crates/api/src/account/account_search_events.rs
@@ -37,7 +37,7 @@ pub async fn account_search_events_controller(
         created_at: body.filter.created_at,
         updated_at: body.filter.updated_at,
         sort: body.sort,
-        limit: body.limit.or(Some(200)), // Default limit to 200
+        limit: body.limit.or(Some(1000)), // Default limit to 1000
     };
 
     execute(usecase, &ctx)
