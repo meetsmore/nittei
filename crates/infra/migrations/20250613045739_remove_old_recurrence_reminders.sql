f234e9c5927d3dde0838ed9129a6750cd5b2fb08
Guillaume Deconinck <deconinck.guillaume@meetsmore.com>
2025-06-13 14:15:30 +0900

    Revert "refactor: remove old columns"
    This reverts commit 68553d19f4f1c028c3874c56003eb6d8f221e79d.

--- crates/infra/migrations/20250613045739_remove_old_recurrence_reminders.sql
+++
@@ -1,7 +0,0 @@
--- Remove `recurrence` and `reminders` columns from `calendar_events` table.
--- We now use `recurrence_jsonb` and `reminders_jsonb` instead.
-ALTER TABLE
-  "calendar_events" DROP COLUMN IF EXISTS "recurrence";
-
-ALTER TABLE
-  "calendar_events" DROP COLUMN IF EXISTS "reminders";
