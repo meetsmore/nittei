3e89b388d2455a9005af475213fc2d4e752d335a
Guillaume Deconinck <113656593+GuillaumeDecMeetsMore@users.noreply.github.com>
2025-06-16 17:27:07 +0900

    fix: migration (#321)
    * fix: migration
    
    * fix: migration (2)
    
    * fix: re-add old columns
    
    * chore: add comments

--- crates/infra/migrations/20250613045739_create_and_use_new_recurrence_indexes.sql
+++ crates/infra/migrations/20250613045739_create_and_use_new_recurrence_indexes.sql
@@ -13,11 +13,3 @@ DROP INDEX IF EXISTS calendar_events__user_uid__start_time__not_null_recurrence;
 
 -- Delete compound index on `user_uid`, `event_type` where `recurrence` is not null (block events)
 DROP INDEX IF EXISTS calendar_events__user_uid__event_type__not_null_recurrence;
-
--- Remove `recurrence` and `reminders` columns from `calendar_events` table.
--- We now use `recurrence_jsonb` and `reminders_jsonb` instead.
-ALTER TABLE
-  "calendar_events" DROP COLUMN IF EXISTS "recurrence";
-
-ALTER TABLE
-  "calendar_events" DROP COLUMN IF EXISTS "reminders";
